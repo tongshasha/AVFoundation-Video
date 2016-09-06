9d98024f255a4d29d4aa4069d5ed6be4d982f4b3
tongshasha <qfnutss@163.com>
2016-09-06 14:32:28 +0800

    Initial Commit

---
+++ AVFoundation-Video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFoundation-Video
+//
+//  Created by 佟莎莎 on 16/9/6.
+//  Copyright © 2016年 asha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AVFoundation-Video/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVFoundation-Video
+//
+//  Created by 佟莎莎 on 16/9/6.
+//  Copyright © 2016年 asha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
